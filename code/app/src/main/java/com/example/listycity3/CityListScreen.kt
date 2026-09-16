28f762ac07115c88fb006c1c43fe0ac0e25843c4
Eliza Lazar <elizalazar.ca@gmail.com>
2026-09-16 16:24:59 -0600

    Implemented Lab Participation Exercise

--- code/app/src/main/java/com/example/listycity3/CityListScreen.kt
+++ code/app/src/main/java/com/example/listycity3/CityListScreen.kt
@@ -1,5 +1,6 @@
 package com.example.listycity3
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -29,20 +30,35 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.foundation.layout.fillMaxSize
 // Lab Participation
 import androidx.compose.foundation.clickable // From 'Hints'
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun CityListScreen(
     cities: List<City>,
     onAddCity: (City)-> Unit, // Part 2, step 3
+    onUpdateCity: (City, City)-> Unit, // Lab Participation, to be able to update the city
     modifier: Modifier = Modifier
 ) {
     // Part 2, step 4
     var newCityName by remember { mutableStateOf("") }
     var newProvinceName by remember { mutableStateOf("") }
     var showAddCityFields by remember { mutableStateOf(false) } // Part 3, step 2
+    var showEditCityFields by remember { mutableStateOf(false) } // Lab Participation - For the edit button
+    var selectCityName by remember { mutableStateOf<String?>(null)} // Lab Participation - For selectability - I took this from my Lab 2 work
+    var selectProvinceName by remember { mutableStateOf<String?>(null)} // Lab Participation - For selectability - I took this from my Lab 2 work
+
 
     // Part 2, step 5
     Column(modifier = modifier.fillMaxSize()) { // Part 3, step 3
+        // Lab Participation, for my own visuals
+        Row(modifier = Modifier.padding(all = 16.dp)){
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "ListyCity v2",
+                fontSize = 40.sp
+            )
+        }
+
         // Part 3, step 4
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -52,13 +68,46 @@ fun CityListScreen(
                 modifier = Modifier.padding(16.dp),
                 onClick = {
                     showAddCityFields = !showAddCityFields
+                    showEditCityFields = false // Lab Participation - Just added an Edit floating action button
+                    // Lab Participate - Setting everything to null
+                    selectCityName = null
+                    selectProvinceName = null
                 }
             ) {
                 Text("+")
             }
+
+            // Lab Participation - Getting an Edit drop down
+            FloatingActionButton(
+                modifier = Modifier.padding(16.dp),
+                onClick = {
+                    showEditCityFields = !showEditCityFields
+                    showAddCityFields = false
+                    selectCityName = null
+                    selectProvinceName = null
+                }
+            ) {
+                Text("Edit")
+            }
         }
+
+
+
         // Part 3, step 5
-        if (showAddCityFields) {
+        if (showAddCityFields or showEditCityFields) { // Updated with Lab Participation to have the showEditCityFields
+            if (showEditCityFields) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    // Lab Participation - Clarification on how to edit
+                    Text(
+                        text = "To edit a city, please select a city/province and then update the name & province through the textboxes.",
+                        fontSize = 12.sp
+                    )
+                }
+            }
             // Part 2, step 6
             Row(
                 modifier = Modifier
@@ -82,29 +131,73 @@ fun CityListScreen(
                 )
                 // Part 2, step 9
                 Spacer(modifier = Modifier.width(8.dp))
-                Button(
-                    modifier = Modifier.padding(vertical = 12.dp),
-                    onClick = {
-                        if (newCityName.isNotBlank() && newProvinceName.isNotBlank()) {
-                            onAddCity(
-                                City(
-                                    name = newCityName,
-                                    province = newProvinceName
+                if (showAddCityFields){ // Lab Participation, just implementing the showAddCityFields portion
+                    Button(
+                        modifier = Modifier.padding(vertical = 12.dp),
+                        onClick = {
+                            if (newCityName.isNotBlank() && newProvinceName.isNotBlank()) {
+                                onAddCity(
+                                    City(
+                                        name = newCityName,
+                                        province = newProvinceName
+                                    )
                                 )
-                            )
-                            newCityName = ""
-                            newProvinceName = ""
-                            showAddCityFields = false // Part 3, step 6
+                                newCityName = ""
+                                newProvinceName = ""
+                                showAddCityFields = false // Part 3, step 6
+                            }
                         }
+                    ) {
+                        Text("Add City")
                     }
-                ) {
-                    Text("Add City")
+                }
+
+                // Updated with Lab Participation to show to showEditCityFields portion
+                if (showEditCityFields){
+                    Button(
+                        modifier = Modifier.padding(vertical = 12.dp),
+                        onClick = {
+                            if (newCityName.isNotBlank() && newProvinceName.isNotBlank()) {
+                                onUpdateCity(
+                                    City(
+                                        name = selectCityName.toString(),
+                                        province = selectProvinceName.toString()
+                                    ),
+                                    City(
+                                        name = newCityName,
+                                        province = newProvinceName
+                                    )
+                                )
+                                newCityName = ""
+                                newProvinceName = ""
+                                showEditCityFields = false
+                            }
+                        }
+                    ) {
+                        Text("Edit City")
+                    }
+
                 }
             }
 
             LazyColumn(modifier = Modifier.fillMaxSize()) { // Part 3, step 7
                 itemsIndexed(cities) { index, city ->
-                    CityRow(city = city)
+                    CityRow(
+                        // Lab Participation - For selectability - I took this from my Lab 2 work
+                        city = city,
+                        selected = ((city.name == selectCityName) and (city.province == selectProvinceName)),
+                        editMode = showEditCityFields, // Lab Participation - Only selectable when in edit mode
+                        onClick = {
+                            if (selectCityName == city.name && selectProvinceName == city.province) {
+                                selectCityName = null
+                                selectProvinceName = null
+                            }
+                            else {
+                                selectCityName = city.name
+                                selectProvinceName = city.province
+                            }
+                        }
+                    )
                     if (index < cities.lastIndex) {
                         HorizontalDivider()
                     }
@@ -115,7 +208,13 @@ fun CityListScreen(
 }
 
 @Composable
-fun CityRow(city: City) {
+fun CityRow(
+    city: City,
+    // Lab Participation - For selectability - I took this from my Lab 2 work
+    selected: Boolean = false,
+    onClick: () -> Unit = {},
+    editMode: Boolean = false, // To only select when on Edit city mode
+) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -124,13 +223,37 @@ fun CityRow(city: City) {
         Text(
             text = city.name,
             fontSize = 30.sp,
-            modifier = Modifier.weight(1f)
+            // Lab Participation - For selectability - I took this from my Lab 2 work
+            modifier = Modifier
+                .weight(1f)
+                .background(
+                    if (selected && editMode) {
+                        Color.Gray
+                    }
+                    else {
+                        Color.White
+                    }
+                )
+                .padding(horizontal = 18.dp, vertical = 14.dp)
+                .clickable { onClick() }
         )
 
         Text(
             text = city.province,
             fontSize = 30.sp,
-            modifier = Modifier.weight(1f)
+            // Lab Participation - For selectability - I took this from my Lab 2 work
+            modifier = Modifier
+                .weight(1f)
+                .background(
+                    if (selected && editMode) {
+                        Color.Gray
+                    }
+                    else {
+                        Color.White
+                    }
+                )
+                .padding(horizontal = 18.dp, vertical = 14.dp)
+                .clickable { onClick() }
         )
     }
 }
@@ -146,7 +269,8 @@ fun CityListScreenPreview() {
                 City("Vancouver", "BC"),
                 City("Calgary", "AB")
             ),
-            onAddCity = {}
+            onAddCity = {},
+            onUpdateCity = {_, _ -> }  // I was having an error with unused parameters: https://medium.com/@dmstocking/kotlin-considered-harmful-unused-parameters-211846add4c0 - Accessed 09-16-2026
         )
     }
 }
--- code/app/src/main/java/com/example/listycity3/MainActivity.kt
+++ code/app/src/main/java/com/example/listycity3/MainActivity.kt
@@ -4,21 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.listycity3.ui.theme.ListyCity3Theme
+// Deleted unused libraries
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +22,10 @@ class MainActivity : ComponentActivity() {
                     CityListScreen(
                         cities = cityRepository.cities,
                         onAddCity = { cityRepository.addCity(it) }, // Part 2, step 2
+                        // Lab Participation
+                        onUpdateCity = { oldCity, updatedCity ->
+                            cityRepository.updateCity(oldCity, updatedCity)
+                                       },
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
